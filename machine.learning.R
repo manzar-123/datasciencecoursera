bf1e7878adf5b007719945a86bf2e595b0ebc771
manzar-123 <s.manzar123@gmail.com>
2020-08-19 15:42:48 +0500

    cvvb

--- machine.learning.R
+++ machine.learning.R
@@ -23,3 +23,37 @@ View(train)
 
 test <- tab[-n,]
 View(test)
+
+fit <- lm(price~engine+horse+weight, data = train)
+pred <- predict(fit)
+trainmse <- mean((train$price - pred)^2)
+trainmse
+
+fit2 <- lm(price~engine+horse+weight, data = test)
+pred2 <- predict(fit2)
+testmse <- mean((test$price - pred2)^2)
+testmse
+
+#another way of upper 3 lines
+pred3 <- predict(fit, test)
+testmse2 <- mean((test$price - pred3)^2)
+testmse2
+
+# k fold validation
+
+install.packages('DAAG')
+require(DAAG)
+library(DAAG)
+
+fit3 <- cv.lm(data=tab, form.lm = formula(price~engine+horse+weight), m=10)
+View(fit3)
+
+
+
+
+
+
+
+
+
+
